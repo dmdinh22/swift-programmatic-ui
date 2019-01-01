8b3e5edbb28e0b13bdd15a5cb4ee4cdbfc7d0405
David Dinh <dmdinh22@gmail.com>
2018-12-31 19:12:41 -0800

    Initial Commit

---
+++ swift-programmatic-ui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-programmatic-ui
+//
+//  Created by David D on 12/31/18.
+//  Copyright © 2018 David D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
