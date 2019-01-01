5d2152950d35bf0bf8c07ac4328cf2703507a8c9
David Dinh <dmdinh22@gmail.com>
2018-12-31 19:22:13 -0800

    get rid of storyboard, create new VC and entry point

---
+++ swift-programmatic-ui/FirstScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstScreenViewController.swift
+//  swift-programmatic-ui
+//
+//  Created by David D on 12/31/18.
+//  Copyright © 2018 David D. All rights reserved.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
