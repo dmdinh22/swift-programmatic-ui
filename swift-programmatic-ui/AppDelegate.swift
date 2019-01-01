666e60c5f0f4738d3b94e4d0c433985ccc0fb8d0
David Dinh <dmdinh22@gmail.com>
2018-12-31 19:32:59 -0800

    use navigation controller

--- swift-programmatic-ui/AppDelegate.swift
+++ swift-programmatic-ui/AppDelegate.swift
@@ -16,8 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let firstScreen = FirstScreenViewController()
+        let navigationController = UINavigationController(rootViewController: firstScreen)
+
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = FirstScreenViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
         return true
